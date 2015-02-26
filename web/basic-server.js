dd93190213aa571f261ba99e1d26f6d7a8a93a5b
dblanchard13 <davidb.prof@gmail.com>
2015-02-25 16:14:49 -0800

    Server running with basice request handlers

--- web/basic-server.js
+++ web/basic-server.js
@@ -8,6 +8,8 @@ initialize();
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+var server = http.createServer(function(req, res){ 
+  handler.handleRequest(req, res)
+});
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,55 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelp = require('./http-helpers');
+var database = require('./database.js').database;
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var defaultCors = httpHelp.headers;
+
+var endResponse = function(response, object, statusCode){
+  statusCode = statusCode || 200;
+  var headers = defaultCors;
+  headers['Content-Type'] = 'text/plain';
+  var result = JSON.stringify(object);
+  response.writeHead(statusCode, headers);
+  response.end(result);
+  // need to make result = archive.paths.list
+}
+
+
+var object = {results: []};
+
+exports.handleRequest = function (request, response) {
+
+  if(request.method === 'POST'){
+    var data = '';
+    request.on('data', function(chunk){
+      data += chunk;
+    });
+    request.on('end', function(){
+      var message = JSON.parse(data);
+
+      database.set(request.url, message, function(){
+        endResponse(respone, object, 201)
+      })
+    });
+  }
+
+  if(request.method === 'GET'){
+    database.get(request.url, function(result){
+      object.results = result;
+      endResponse(response, object, 200);
+    });
+  }
+
+  if(request.method === 'OPTIONS'){
+    endResponse(response, {}, 200);
+  }
+
+
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  // res.end(archive.paths.list);
+
 };
+
+// exports.handleRequest = handleRequest;
